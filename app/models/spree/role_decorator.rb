07ebc20595145f3f75a894bbbdd3eba8e0c943b6
Jose Alvarez <alvarezloaiciga@gmail.com>
2016-04-20 11:57:43 -0600

    Role decorator syntax change

--- app/models/spree/role_decorator.rb
+++ app/models/spree/role_decorator.rb
@@ -1,6 +1,4 @@
-module Spree
-  Role.class_eval do
-    has_many :price_books
-    scope :with_price_book, -> { where(id: Spree::PriceBook.pluck(:role_id).uniq) }
-  end
+Spree::Role.class_eval do
+  has_many :price_books
+  scope :with_price_book, -> { where(id: Spree::PriceBook.pluck(:role_id).uniq) }
 end
